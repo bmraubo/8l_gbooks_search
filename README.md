3f2c0fa24c307ce73b19aa695baf29c5747d07fd
QXCross <bmraubo@gmail.com>
2021-06-12 12:13:59 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # 8l_gbooks_search
 Test Assessment for 8 Light
+
+Command line application that allows you to use the Google Books API to search for books and construct a reading list.
+
+This application should allow you to:
+
+Type in a query and display a list of 5 books matching that query.
+
+Each item in the list should include the book's author, title, and publishing company.
+
+A user should be able to select a book from the five displayed to save to a “Reading List”
+
+View a “Reading List” with all the books the user has selected from their queries -- this is a local reading list and not tied to Google Books’s account features.
+
---
+++ app.js
@@ -0,0 +1,28 @@
+/////Help parameter to explain options - there is only one - 'View reading list'
+
+///// Search function
+
+//Type in query in console
+
+//Send Request through Google Books API
+
+//Print list of top 5 results
+    //Display books author, title, publishing company
+    //Use colors to make key information pop
+    //Use inquirer to print check boxes next to each result
+
+//Offer user information on next step to save book to Reading list
+
+///// Save function
+
+//Use JSON to store information
+
+//Allow user to select book from search results
+    //This is done using inqurier checkboxes
+
+//Save selected
+
+/////View function
+
+//Include launch parameter to view reading list
+
